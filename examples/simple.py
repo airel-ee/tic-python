777303635f3537368500dd8c5fed4cc98da6d476
Sander Mirme <sander.mirme@ut.ee>
2022-11-03 14:40:59 +0200

    Add example programs

---
+++ examples/simple.py
@@ -0,0 +1,24 @@
+import airel.tic
+
+print("Connecting")
+device = airel.tic.Tic()
+
+print("Updating settings")
+device.reset_settings({
+    "auto_zero_interval": 300.0,
+    "auto_zero_duration": 60.0,
+    "auto_zero_enabled": True,
+    "extended_record_fields_enabled": True,
+    "non_run_records_hidden": False,
+    "averaging_period": 10.0,
+})
+
+print("Acive settings:", device.get_settings())
+
+print("Start measurements")
+device.set_mode("run")
+
+while True:
+    msg = device.receive_message(1.0)
+    if msg is not None:
+        print(msg)
